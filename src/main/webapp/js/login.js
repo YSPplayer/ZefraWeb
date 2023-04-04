72cd99147eecec6235c0396e25b22ee7a0042331
Ha <479310608@qq.com>
2023-04-04 16:32:49 +0800

    图片反转动画效果完成

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,15 +1,63 @@
+//正面图片 
+var godstate = true;
+var vue_login_main_img_front = new Vue({
+    el:"#_login_main_img_front",
+    data:function () {
+        return { 
+            src:"",
+        }
+    },
+}); 
+//背面图片
+var vue_login_main_img_back = new Vue({
+    el:"#_login_main_img_back",
+    data:function () {
+        return { 
+            src:"",
+        }
+    },
+}); 
+//检查zefra_divine数据是否被使用，就是图片不重置
+var checkDate = function() {
+    var msg = localStorage.getItem("zefra_divine");
+    if(!msg) return false;
+    var data = JSON.parse(msg);
+    ZfraObjects.loginIndex = data.index;
+    var willtime = data.time + ZfraObjects.ms;
+    return Date.parse(new Date()) < willtime ? true : false;
+
+}
+//对网页进行初始化操作，一些动画的初始化
+var initialize = function() {
+    //移除img上的class，使其加载图片时不播放动画
+    ZfraTools.removeClass(document.getElementById("_flipper"),"class_animation_filp");
+    ZfraTools.removeClass(document.getElementById("_login_main_text"),"class_animation_emergence");
+    ZfraTools.removeAllClass(document.getElementById("_login_main_img_front"));
+    ZfraTools.removeAllClass(document.getElementById("_login_main_img_back"));
+    //如果我们占卜过，网页加载时只给定占卜的动画
+    checkDate() ? Vue.set(vue_login_main_img_front,"src",`pics/login/${ZfraObjects.loginArr[ZfraObjects.loginIndex]}.jpg`) :
+                 Vue.set(vue_login_main_img_front,"src","pics/login/divine.jpg");
+    
+
+}; 
 window.onload = function() {
-    var _login_main_imag = document.getElementById("_login_main_img");
+    initialize();
+    var _login_main_imag_front = document.getElementById("_login_main_img_front");
+    var _login_main_imag_back = document.getElementById("_login_main_img_back");
+    var _flipper = document.getElementById("_flipper");
+    var _login_main_text = document.getElementById("_login_main_text");
     //绑定鼠标悬停在图片之上的事件
-    _login_main_imag.onmouseover = async function() {
+    _login_main_imag_front.onmouseover = async function() {
         /*
             如果我们在这里写一个死循环，那么即使这个事件结束掉这个函数依然是在执行的
             这个本质上也是开了异步线程的函数
         */
+        if(checkDate) return;
+        ZfraTools.rebroadcast(this,"class_animation_enlarge",true);
         ZfraObjects.isCv = true;
-            var x = ZfraTools.toString(ZfraTools.getRandomNumber(0,256));
-            var y = ZfraTools.toString(ZfraTools.getRandomNumber(0,256));
-            var z = ZfraTools.toString(ZfraTools.getRandomNumber(0,256));
+            var x = ZfraTools.toString(ZfraTools.getRandomNumber(0,256)),
+                y = ZfraTools.toString(ZfraTools.getRandomNumber(0,256)),
+                z = ZfraTools.toString(ZfraTools.getRandomNumber(0,256));
             var o = 0;
             var flag = false;
             var speed = 1.6;
@@ -27,17 +75,42 @@ window.onload = function() {
             this.style.boxShadow = `0px 0px ${o}px rgb(${x}, ${y}, ${z})`;
             await ZfraTools.sleep(29);
         };
-        this.style.boxShadow = ZfraObjects.boxShadow;
+        this.style.boxShadow = "none";
     };
     //绑定鼠标离开悬停图的事件
-    _login_main_imag.onmouseout = async function() {
-        ZfraObjects.isCv = false; 
+    _login_main_imag_front.onmouseout = async function() {
+        if(checkDate) return;
+        ZfraObjects.isCv = false;
+        if(!ZfraObjects.isClick) {
+           ZfraTools.rebroadcast(this,"class_animation_reduce",true);
+        }
     };
-    _login_main_imag.onclick = async function() {
-        //perspective(800px) rotateX(0deg) rotateY(150deg)
-        this.style.transform = 'rotateY(150deg)';
-        //_login_main_imag.style.transform
-        //transform: rotateY(150deg);
-        //oWrap.style.transform='perspective(800px) rotateX('+roX+'deg) rotateY('+roY+'deg)';
-    }
+    //单机Img图片时触发的事件
+    _flipper.addEventListener("click", async function(e){
+        if(checkDate && !godstate) return;
+        else {
+            //localStorage.removeItem("zefra_divine");
+        }
+        e.preventDefault;
+        ZfraObjects.isClick = true;
+        //事件点击时重播动画，旋转图片
+        this.style.animationFillMode = "forwards";
+        //在翻转卡片之前，通过随机数给我们的卡片增加图片;
+        var index = ZfraTools.getRandomIntegerNumber(0,ZfraObjects.loginArr.length);
+        if(index >= ZfraObjects.loginArr.length || index < 0) index = 0;
+        Vue.set(vue_login_main_img_back,"src",`pics/login/${ZfraObjects.loginArr[index]}.jpg`);
+        ZfraTools.rebroadcast(this,"class_animation_filp",false);
+        ZfraTools.rebroadcast(_login_main_imag_back,"class_animation_enlarge",true);
+        await ZfraTools.sleep(2500);
+        ZfraTools.rebroadcast(_login_main_imag_back,"class_animation_reduce",true);
+        ZfraObjects.isClick = false;
+        var data = new Object;
+        data.time = Date.parse(new Date());
+        data.index = index;
+        var msg = JSON.stringify(data);
+        //把当前时间的存入本地
+        localStorage.setItem("zefra_divine",msg);
+        /*放置好后加载我们的文本动画*/
+        ZfraTools.rebroadcast(_login_main_text,"class_animation_emergence",false);
+    });
 };
--- src/main/webapp/js/tools.js
+++ src/main/webapp/js/tools.js
@@ -2,8 +2,29 @@
 存放js所用到的一些工具方法，不包含页面的执行逻辑
 */
 var ZfraObjects = {
-    boxShadow:"none",/*设置图片的默认显示*/
-    isCv:false/*设置一个变量，以跳出随机颜色的循环*/
+    isCv:false,/*设置一个变量，以跳出随机颜色的循环*/
+    isClick:false,//如果我们点击了图片按钮就不执行图片的缩放动画事件
+    ms:86400000, //一天的毫秒数
+    loginIndex:0,
+    loginArr: new Array("bule","gold","green","orange","purple","red"),//新建图片的数组库
+    colorArr: new Array( "蓝色","黄色","绿色","橙色","紫色","红色"),
+    desArr: new Array(
+        "抽到这张卡的今天，运气超级差！",
+        "抽到这张卡的今天，运气非常不好！",
+        "抽到这张卡的今天，运气不太好！",
+        "抽到这张卡的今天，运气马马虎虎！",
+        "抽到这张卡的今天，运气非常好！",
+        "抽到这张卡的今天，运气超级好！"
+    ),
+    desArr2: new Array(
+        "也许会被竞争对手超越！加油！！",
+        "小心弄丢东西！",
+        "向东可能是幸运的朝向。",
+        "也许能找到丢失的东西。",
+        "可能会有快乐的事?",
+        "任何愿望都能实现！"
+    ),
+    luckyArr: new Array( "闪光饰物","金鱼","植物","伞","墨镜","皮鞋"), 
 }
 var ZfraTools = {
     //让当前函数休眠time秒
@@ -19,10 +40,57 @@ var ZfraTools = {
     getRandomNumber:function(min,max) {
         return Math.random()*(max - min + 1) + min;
     },
+    //获取min-max之间的一个随机整数，包括min不包括max
+    getRandomIntegerNumber:function(min,max) {
+        var key = Math.round(Math.random()*(max - min + 1) + min);
+        return key >= max ? key - 1 : key; 
+    },
     //toString方法
     toString:function(o) {
         return o.toString();
-    }
-
+    },
+    //给对象增加属性名
+    addClass:function(obj, cls) {
+        var obj_class = obj.className,//获取 class 内容.
+        blank = (obj_class != '') ? ' ' : '';//判断获取到的 class 是否为空, 如果不为空在前面加个'空格'.
+        added = obj_class + blank + cls;//组合原来的 class 和需要添加的 class.
+        obj.className = added;//替换原来的 class.
+    },
+     //判断对象是否有属性名
+    hasClass:function(obj, cls) {
+        var obj_class = obj.className,//获取 class 内容.
+        obj_class_lst = obj_class.split(/\s+/),//通过split空字符将cls转换成数组.
+        x = 0;
+        for(x in obj_class_lst) {
+            if(obj_class_lst[x] == cls) {//循环数组, 判断是否包含cls
+            return true;
+            }
+        }
+        return false;
+    }, 
+    //移除对象的类名
+    removeClass:function(obj, cls) {
+        var obj_class = ' '+obj.className+' ';//获取 class 内容, 并在首尾各加一个空格. ex) 'abc    bcd' -> ' abc    bcd '
+        obj_class = obj_class.replace(/(\s+)/gi, ' '),//将多余的空字符替换成一个空格. ex) ' abc    bcd ' -> ' abc bcd '
+        removed = obj_class.replace(' '+cls+' ', ' ');//在原来的 class 替换掉首尾加了空格的 class. ex) ' abc bcd ' -> 'bcd '
+        removed = removed.replace(/(^\s+)|(\s+$)/g, '');//去掉首尾空格. ex) 'bcd ' -> 'bcd'
+        obj.className = removed;//替换原来的 class.
+    },
+    //移出对象的所有类名
+    removeAllClass:function(obj) {
+        obj.className = "";
+    },
+    //对指定标签下的动画执行重播，实现原理就是移除类然后再添加类重新加载
+    rebroadcast:function(obj,cls,flag) {
+     // 1、删除动画的class，如果flag为true删除全部类防止动画的播放冲突
+     flag ? this.removeAllClass(obj) : this.removeClass(obj,cls);
+     // 2、改变元素的offsetWidth
+     var temp = obj.offsetWidth;
+     obj.offsetWidth = 0;
+     obj.offsetWidth = temp;
+     // 3、重新添加动画的class
+     this.addClass(obj,cls);
+    },
+    
 };
 
